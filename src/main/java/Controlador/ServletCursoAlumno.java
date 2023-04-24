37629d5944cba0df2dcb767da6015f549c0f55d6
Holly <holly.cg@outlook.com>
2023-04-23 19:40:49 -0600

    new changes

--- src/main/java/Controlador/ServletCursoAlumno.java
+++ src/main/java/Controlador/ServletCursoAlumno.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/ServletCursos")
+@WebServlet("/ServletCursoAlumno")
 public class ServletCursoAlumno extends HttpServlet {
     private static final long serialVersionUID = 1L;
     @Override
     protected void doGet(HttpServletRequest hsreq, HttpServletResponse hsres)
             throws ServletException, IOException {
         List<CursoAlumno> cursos = new CursoAlumnoDAO().listar();
-        System.out.println("cursos_alumno = " +cursos);
+        System.out.println("cursosalumno = " +cursos);
         hsreq.setAttribute("cursoalumno", cursos);
         hsreq.getRequestDispatcher("cursoalumno.jsp").forward(hsreq, hsres);
     }
